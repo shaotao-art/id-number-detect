d144f83b5d1e1eba56a74f5793cbefe229f8f824
starfish <2842226412@qq.com>
2022-10-27 12:05:35 +0800

    add argparse

--- get_photo.py
+++ get_photo.py
@@ -1,12 +1,19 @@
 from picamera import PiCamera
 from time import sleep
+import argparse
 
-def get_photo(file_name):
+def get_photo():
+    args = argparse.ArgumentParser()
+    args.add_argument('--save_path', '-p', type=str, required=True, help="输入照片存储路径")
+    args_dict = vars(args.parse_args())
+
     print('starting getting photo....')
     camera = PiCamera()
-
     camera.start_preview()
     sleep(5)
-    camera.capture(file_name)
+    camera.capture(args_dict['save_path'])
     camera.stop_preview()
-    print(f'save photo to {file_name}, done!')
+    print(f'save photo to {args_dict["save_path"]}, done!')
+
+if __name__ == "__main__":
+    get_photo()
--- run.py
+++ run.py
@@ -1,16 +1,23 @@
-
-from get_photo import get_photo
 from pre_process import get_numbers
 from getColumnNum import get_each_number
 from infer import recognize_numbers
+import os
+import argparse
 
 def run():
-    origin_file_path = './TEST_IMG/test1.jpg'
-    id_numbers_path = './TEST_IMG/out1.jpg'
-    each_number_save_path = './number_cut_out'
-    get_photo(file_name = origin_file_path)
-    get_numbers(file_path = origin_file_path, save_path = id_numbers_path, use_rotate = False)
-    numbers_lst = get_each_number(path = id_numbers_path, save_path = each_number_save_path)
+    args = argparse.ArgumentParser()
+    args.add_argument('--img_path', '-p', type=str, required=True, help="输入身份证照片路径")
+    args_dict = vars(args.parse_args())
+    
+    tmp_dir = './tmp_run'
+    id_numbers_path = os.path.join(tmp_dir, 'id_numbers.jpg')
+    each_number_save_path = os.path.join(tmp_dir, 'numbers')
+
+    os.makedirs(tmp_dir, exist_ok=True)
+    os.makedirs(each_number_save_path, exist_ok=True)
+
+    get_numbers(file_path = args_dict['img_path'], save_path = id_numbers_path, use_rotate = False)
+    get_each_number(path = id_numbers_path, save_path = each_number_save_path)
     recognize_numbers(each_number_save_path)
 
 if __name__ == "__main__":
