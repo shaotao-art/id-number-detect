656e53b1e3ffaf769765221e952a644dee7f8f0d
starfish <2842226412@qq.com>
2022-10-27 09:10:36 +0800

    rename infer_pre to infer.py

---
+++ infer.py
@@ -0,0 +1,53 @@
+import torch
+from torchvision import transforms
+from number_recog.src.model import NNet
+from PIL import Image
+import os
+
+
+def get_T():
+    T = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+    return T
+
+
+def get_model(model_save_path = './ckp/model.ckp'):
+    checkpoint = torch.load(model_save_path, map_location='cpu')
+    model = NNet()
+    model.load_state_dict(checkpoint['model'])
+    return model
+
+
+def prepare_imgs(T, root_path):
+    out_lst = []
+    file_num = len(os.listdir(root_path))
+    assert file_num == 18, 'not enough img to infer'
+    for i in range(18):
+        file_path = os.path.join(root_path, f"img_{i}.jpg")
+        img = Image.open(file_path)
+        img = T(img)
+        out_lst.append(img.unsqueeze(0))
+    return torch.cat(out_lst)
+
+
+def infer(model, img):
+    pred = model(img)
+    res = pred.argmax(1)
+    return res
+
+def recognize_numbers(root_path):
+    model = get_model()
+    T = get_T()
+    imgs = prepare_imgs(T, root_path)
+    res = infer(model, imgs)
+    assert res.shape[-1] == 18, f"should get 18 numbers, but get {res.shape}"
+    lst = [res[i].item() for i in range(18)]
+    print('----- final res -----')
+    print(f'your id number is {lst}')
+    print('----- final res -----')
+
+if __name__ == "__main__":
+    recognize_numbers('./number_cut_out')
